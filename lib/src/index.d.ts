2643d02edfd3032313f434b6310d70783fb0e7eb
callistonianembrace <87455417+callistonianembrace@users.noreply.github.com>
2022-10-21 14:46:17 +0530

    Initial commit

---
+++ lib/src/index.d.ts
@@ -0,0 +1,11 @@
+import { Logger, IPluginMiddleware, PluginOptions } from "@verdaccio/types";
+import { Client } from "pg";
+import { Application } from "express";
+import { CustomConfig } from "../types/index";
+export declare function addScope(scope: string, packageName: string): string;
+export default class VerdaccioMiddlewarePlugin implements IPluginMiddleware<CustomConfig> {
+    logger: Logger;
+    client: Client;
+    constructor(config: CustomConfig, options: PluginOptions<CustomConfig>);
+    register_middlewares(app: Application): void;
+}
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { Logger, IPluginMiddleware, PluginOptions } from "@verdaccio/types";
+import { Client } from "pg";
+import debugF from "debug";
+
+const debug = debugF("verdaccio:plugin:ligo-registry:examplegen");
+
+import { Router, Request, Response, NextFunction, Application } from "express";
+
+import { CustomConfig } from "../types/index";
+export function addScope(scope: string, packageName: string): string {
+  return `@${scope}/${packageName}`;
+}
+
+export default class VerdaccioMiddlewarePlugin
+  implements IPluginMiddleware<CustomConfig>
+{
+  public logger: Logger;
+  public client: Client;
+  public constructor(options: PluginOptions<CustomConfig>) {
+    this.logger = options.logger;
+    this.client = new Client();
+  }
+  public register_middlewares(app: Application): void {
+    debug("Registering");
+    try {
+      const router = Router();
+      this.client.connect();
+      router.get(
+        "/examplegen/(@:scope/)?:packageName/:version",
+        async (
+          request: Request,
+          response: Response & { report_error?: Function },
+          next: NextFunction
+        ): Promise<void> => {
+          const packageName = request.params.scope
+            ? addScope(request.params.scope, request.params.packageName)
+            : request.params.package;
+          const { version } = request.params;
+          debug(packageName, version);
+          let res = await this.client.query(
+            "SELECT * FROM publish WHERE name = $1 and version = $2",
+            [packageName, version]
+          );
+          response.status(200).json(res.rows);
+        }
+      );
+      app.use("/", router);
+      debug("registered");
+    } catch (e) {
+      this.client.end();
+      debug(
+        "Failed to establish connection to postgres. Not registering verdaccio-ligo-registry-examplegen plugin"
+      );
+      debug(e);
+    } finally {
+    }
+  }
+}
